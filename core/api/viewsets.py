120575fc9871693945246c69433b38805f08bda4
superensino <superensino@superensino-Latitude-5490>
2019-05-09 14:29:59 -0400

    subscrevendo actions do rest

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,8 +1,39 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
+
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        return PontoTuristico.objects.filter(aprovado=True)
+
+    """def list(self, request, *args, **kwargs):
+        return Response ({'teste':123})"""
+
+    """def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data['nome']})
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get', 'post'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
+        pass"""
